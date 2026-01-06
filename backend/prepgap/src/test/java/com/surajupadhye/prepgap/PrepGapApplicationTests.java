fa6ba35904ea946090892164d5d73d9aa33a1e09
Suraj Upadhye <s.upadhye6782@gmail.com>
2026-01-06 09:27:28 +0530

    Initial Spring Boot setup for PrepGap

---
+++ backend/prepgap/src/test/java/com/surajupadhye/prepgap/PrepGapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.surajupadhye.prepgap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrepGapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
